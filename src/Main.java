d1077b2f07078b9bec74b52d271158bbd03823d3
OmarAgamez <odagamez@unicesar.edu.co>
2026-05-04 15:31:44 -0500

    primer commit: agregue un comentario de prueba despues de public class Main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+    // Desarrollado por: Omar Agamez - primer commit
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
